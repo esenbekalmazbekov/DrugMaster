d5144ea7897558506601da6b6cf49062db1c5c68
esenbek <esenbek.almazbekov@gmail.com>
2020-02-13 19:54:46 +0600

    login is finish

--- app/src/main/java/com/example/drugmaster/MainActivity.java
+++ app/src/main/java/com/example/drugmaster/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.drugmaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import com.example.drugmaster.View.GradientTextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    GradientTextView welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        preAnimation();
+    }
+
+    private void preAnimation(){
+        welcome = findViewById(R.id.welcome_id);
+        Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.alpha_anim);
+        welcome.startAnimation(anim);
     }
 }
---
+++ app/src/main/java/com/example/drugmaster/View/GradientTextView.java
@@ -0,0 +1,46 @@
+package com.example.drugmaster.View;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
+import com.example.drugmaster.R;
+
+
+/**
+ * Created by rahulchowdhury on 04/08/16.
+ */
+@SuppressLint("AppCompatCustomView")
+public class GradientTextView extends TextView {
+
+    public GradientTextView(Context context) {
+        super(context);
+    }
+
+    public GradientTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GradientTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @SuppressLint("DrawAllocation")
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        super.onLayout(changed, left, top, right, bottom);
+
+        //Setting the gradient if layout is changed
+        if (changed) {
+            getPaint().setShader(new LinearGradient(0, 0, getWidth(), getHeight(),
+                    ContextCompat.getColor(getContext(), R.color.first),
+                    ContextCompat.getColor(getContext(), R.color.last),
+                    Shader.TileMode.CLAMP));
+        }
+    }
+}
