a3c87d034ef56c41b7e992e1e40fbabe0b72f658
esenbek <esenbek.almazbekov@gmail.com>
2020-02-14 02:29:30 +0600

    before adding image

--- app/src/main/java/com/example/drugmaster/MainActivity.java
+++ app/src/main/java/com/example/drugmaster/MainActivity.java
@@ -2,18 +2,36 @@ package com.example.drugmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
 import com.example.drugmaster.View.GradientTextView;
 
 public class MainActivity extends AppCompatActivity {
     GradientTextView welcome;
+    Button registrBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         preAnimation();
+        registration();
+    }
+
+    private void registration() {
+        registrBtn = findViewById(R.id.registration);
+        registrBtn.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View v) {
+                                              Intent intent = new Intent(MainActivity.this, Registration.class);
+                                              startActivity(intent);
+                                          }
+                                      }
+        );
     }
 
     private void preAnimation(){
